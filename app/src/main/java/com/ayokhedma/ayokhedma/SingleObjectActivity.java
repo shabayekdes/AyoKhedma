b18651ddd2164fa2acadfdccf7a9c0c01d829535
Esmail Shabayek <esmail.shabayek@gmail.com>
2017-03-27 02:04:40 +0200

    Add Activity and Image

---
+++ app/src/main/java/com/ayokhedma/ayokhedma/SingleObjectActivity.java
@@ -0,0 +1,13 @@
+package com.ayokhedma.ayokhedma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleObjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_object);
+    }
+}
