4791707331d0bc0ea1ace00e486c5fe5420318e6
Esmail Shabayek <esmail.shabayek@gmail.com>
2017-04-02 01:23:32 +0200

    Add custome adapter and asynctask

---
+++ app/src/main/java/com/ayokhedma/ayokhedma/CustomeAsync.java
@@ -0,0 +1,14 @@
+package com.ayokhedma.ayokhedma;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by SevenTG on 02/04/2017.
+ */
+
+public class CustomeAsync extends AsyncTask<String,String,String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
