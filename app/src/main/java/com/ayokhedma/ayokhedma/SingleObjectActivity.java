a1994a1dcda1871296860db004636a7790f2470e
kamel912 <mk.25390@gmail.com>
2017-04-02 02:11:49 +0200

    رفع الmain menu

--- app/src/main/java/com/ayokhedma/ayokhedma/SingleObjectActivity.java
+++ app/src/main/java/com/ayokhedma/ayokhedma/SingleObjectActivity.java
@@ -2,6 +2,7 @@ package com.ayokhedma.ayokhedma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class SingleObjectActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class SingleObjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_object);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
