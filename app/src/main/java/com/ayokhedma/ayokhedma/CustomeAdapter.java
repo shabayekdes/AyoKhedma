e9cee7adec4565226df092c16a34a06b421e0361
Esmail Shabayek <esmail.shabayek@gmail.com>
2017-04-04 02:16:20 +0200

    Add Custome Adapter

--- app/src/main/java/com/ayokhedma/ayokhedma/CustomeAdapter.java
+++ app/src/main/java/com/ayokhedma/ayokhedma/CustomeAdapter.java
@@ -1,23 +1,37 @@
 package com.ayokhedma.ayokhedma;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ayokhedma.ayokhedma.models.CategoryModel;
+
+import java.util.ArrayList;
 
 /**
  * Created by SevenTG on 02/04/2017.
  */
 
 public class CustomeAdapter extends BaseAdapter {
+    private ArrayList<CategoryModel> modelArrayList ;
+    private LayoutInflater layoutInflater;
+    public CustomeAdapter(Context context, ArrayList<CategoryModel> modelArrayList){
+        this.layoutInflater = LayoutInflater.from(context);
+        this.modelArrayList = modelArrayList;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return modelArrayList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return modelArrayList.get(i);
     }
 
     @Override
@@ -27,6 +41,11 @@ public class CustomeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        view = layoutInflater.inflate(R.layout.listview,null);
+        TextView txtgrid = (TextView) view.findViewById(R.id.txt_grid);
+        ImageView imggrid = (ImageView) view.findViewById(R.id.img_grid);
+
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/ayokhedma/ayokhedma/models/CategoryModel.java
@@ -0,0 +1,15 @@
+package com.ayokhedma.ayokhedma.models;
+
+/**
+ * Created by SevenTG on 04/04/2017.
+ */
+
+public class CategoryModel {
+    private String name;
+    private int img;
+
+    public CategoryModel(String name, int img) {
+        this.name = name;
+        this.img = img;
+    }
+}
