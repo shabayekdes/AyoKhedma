4f32149f9b4b1f6a0db04e3a7ce8f58b3fb76491
kamel912 <mk.25390@gmail.com>
2017-04-20 13:01:52 +0200

    add searchable activity

--- app/src/main/java/com/ayokhedma/ayokhedma/MainActivity.java
+++ app/src/main/java/com/ayokhedma/ayokhedma/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ayokhedma.ayokhedma;
 
 import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,7 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.SearchView;
+import android.support.v7.widget.SearchView;
 import android.widget.Toast;
 
 import com.ayokhedma.ayokhedma.models.CategoryModel;
@@ -47,9 +49,16 @@ public class MainActivity extends AppCompatActivity implements TaskListener{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu);
-        MenuItem searchItem = (MenuItem) menu.findItem(R.id.search_item);
-     //   SearchManager searchManager = new SearchManager();
-        return super.onCreateOptionsMenu(menu);
+       MenuItem searchItem = menu.findItem(R.id.search_item);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setIconified(true);
+        }
+        return true;
     }
 
     @Override
--- app/src/main/java/com/ayokhedma/ayokhedma/SearchableActivity.java
+++ app/src/main/java/com/ayokhedma/ayokhedma/SearchableActivity.java
@@ -1,13 +1,44 @@
 package com.ayokhedma.ayokhedma;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.List;
 
 public class SearchableActivity extends AppCompatActivity {
-
+    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
+       /* handleIntent(getIntent());
+        SearchManager searchManager = (SearchManager) this.getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) findViewById(R.id.search_view);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconified(false);*/
+
     }
+
+
+
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            if (searchView != null) {
+                searchView.clearFocus();
+            }
+            new CustomeAsync(this).execute();
+        }
+
+    }
+
 }
